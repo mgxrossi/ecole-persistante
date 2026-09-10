d6f40f66e011c02f66980b1a2d887001cfdf7c2c
yoyofrizou <margaux.rossi18@gmail.com>
2026-09-10 14:36:43 +0200

    dao.py comments

--- ecole/ecole/daos/dao.py
+++ ecole/ecole/daos/dao.py
@@ -1,17 +1,26 @@
 # -*- coding: utf-8 -*-
 
 """
+Ça garantit une cohérence : n'importe qui lisant votre code sait,
+rien qu'en voyant class CourseDao(Dao[Course]):, que cette classe aura
+forcément les 4 méthodes create, read, update, delete, avec les bonnes
+signatures. C'est un peu comme une checklist imposée — impossible
+d'oublier une méthode par mégarde, Python vous le signalerait
+immédiatement à l'exécution
 Classe abstraite générique Dao[T], dont hérite les classes de DAO de chaque entité
 """
 
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
+#servent à créer une classe qu'on ne peut jamais utiliser directement, mais qui sert uniquement de modèle pour d'autres classes
 from typing import ClassVar, Optional
 import pymysql.cursors
 
 
 @dataclass
 class Dao[T](ABC):
+
+    #la connexion partagée vers MySQL, ouverte une seule fois, utilisée par tous les DAO
     connection: ClassVar[pymysql.Connection] = \
         pymysql.connect(host='localhost',
                         user='ecole',
@@ -20,6 +29,8 @@ class Dao[T](ABC):
                         cursorclass=pymysql.cursors.DictCursor)
 
     @abstractmethod
+    #@abstractmethod : ça déclare que toute classe qui hérite de Dao doit obligatoirement écrire sa propre version de create()
+
     def create(self, obj: T) -> int:
         """Crée l'entité en BD correspondant à l'objet obj
 
@@ -27,6 +38,7 @@ class Dao[T](ABC):
         :return: l'id de l'entité insérée en BD (0 si la création a échoué)
         """
         ...
+    # Dao ne sait pas comment créer un Course ou un Teacher, chaque sous-classe (CourseDao, TeacherDao...) doit fournir sa propre implémentation concrète
 
     @abstractmethod
     def read(self, id_entity: int) -> Optional[T]:
